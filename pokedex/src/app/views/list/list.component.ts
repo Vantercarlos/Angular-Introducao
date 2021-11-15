a41bb768f3c71167572366e8bd8cf49da96d160a
Vantercarlos Quaresma Caitano <vantercarlosc@l3.com.br>
2021-11-15 16:24:43 -0300

    Adicionado o componente serviçe

--- pokedex/src/app/views/list/list.component.ts
+++ pokedex/src/app/views/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from 'src/app/services/pokeapi.service';
 
 @Component({
   selector: 'app-list',
@@ -7,19 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  nameFilter = '';
   selectedPkm: any = null;
-  pokemonList = [
-    { name: 'Bulbasauro', number: 1 },
-    { name: 'Charmander', number: 4 },
-    { name: 'Squirtle', number: 7 },
-    { name: 'Pikachu', number: 25 }
-  ]
+  get pokemonList(){
+    return this.pokeapi.pokemonList.filter(pokemon => {
+      return pokemon.name.toLowerCase().indexOf(this.nameFilter.toLowerCase()) !== -1;
+    })
+  }
   get pkmSprite(){
     const number = ('000' + this.selectedPkm.number).slice(-3);
     return `//serebii.net/sunmoon/pokemon/${number}.png`;
   }
 
-  constructor() { }
+  constructor(
+    private pokeapi: PokeapiService
+  ) { }
 
   ngOnInit(): void {
   }
