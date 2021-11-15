328eba87577947535facabae6718fc3ef686be01
Vantercarlos Quaresma Caitano <vantercarlosc@l3.com.br>
2021-11-15 12:31:48 -0300

    Selecionar o pokemon via evento

--- pokedex/src/app/views/list/list.component.ts
+++ pokedex/src/app/views/list/list.component.ts
@@ -7,16 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  selectedPkm: any = null;
   pokemonList = [
     { name: 'Bulbasauro', number: 1 },
     { name: 'Charmander', number: 4 },
     { name: 'Squirtle', number: 7 },
     { name: 'Pikachu', number: 25 }
   ]
+  get pkmSprite(){
+    const number = ('000' + this.selectedPkm.number).slice(-3);
+    return `//serebii.net/sunmoon/pokemon/${number}.png`;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectPokemon(pkm: any){
+    this.selectedPkm = pkm;
   }
 
 }
